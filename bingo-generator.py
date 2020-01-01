295b227fead27aef51b7a84c98f731a7ea2db850
chjo <dame-el-poder@web.de>
2020-01-01 20:12:05 +0100

    Modified original bingo custom word generator. + works now with Python3 + removed unnecessary CLI + print first word bold in first line + fields are now ordered according to original BINGO

--- bingo-generator.py
+++ bingo-generator.py
@@ -1,57 +1,90 @@
 #!/usr/bin/env python
 
-import random, sys
+import random
+import math
+import os
 
-# check for the right # of args
-if len(sys.argv) != 4:
-    print "USAGE: " + sys.argv[0], " [file of terms] [output file] [# of cards]"
-    print "Example: " + sys.argv[0] + " bingo_terms.txt bingo.html 20"
-    sys.exit(1)
+########################
+# Settings
+in_file_name = "sample_terms.txt"
+cards = 12
+########################
 
-# read in the bingo terms
-in_file = open(sys.argv[1], 'r')
-terms = [line.strip() for line in in_file.readlines()]
-terms = filter(lambda x: x != "", terms)
+current_workdir = os.path.dirname(os.path.realpath(__file__))
+in_file_path = os.path.join(current_workdir, in_file_name)
+out_file_path = "{}.html".format(os.path.splitext(in_file_path)[0])
+
+# read in the bingo card_entries
+in_file = open(in_file_path, "r")
+
+card_entries = [line.strip() for line in in_file.readlines()]
+
 in_file.close()
 
-# XHTML4 Strict, y'all!
-head = ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n"
-        "<html lang=\"en\">\n<head>\n"
-        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
-        "<title>Bingo Cards</title>\n"
-        "<style type=\"text/css\">\n"
-        "\tbody { font-size: 12px; }\n"
-        "\ttable { margin: 40px auto; border-spacing: 2px; }\n"
-        "\t.newpage { page-break-after:always; }\n"
-        "\ttr { height: 60px; }\n"
-        "\ttd { text-align: center; border: thin black solid; padding: 10px; width: 60px; }\n"
-        "</style>\n</head>\n<body>\n")
 
-# Generates an HTML table representation of the bingo card for terms
-def generateTable(terms, pagebreak = True):
-    ts = terms[:12] + ["FREE SPACE"] + terms[12:24]
+# Generates an HTML table representation of the bingo card for card_entries
+def generate_card(card_entries, pagebreak=True):
+    # ts = card_entries[:12] + ["FREE SPACE"] + card_entries[12:24]
     if pagebreak:
-        res = "<table class=\"newpage\">\n"
+        res = '<table class="newpage">\n'
     else:
         res = "<table>\n"
-    for i, term in enumerate(ts):
-        if i % 5 == 0:
-            res += "\t<tr>\n"
-        res += "\t\t<td>" + term + "</td>\n"
-        if i % 5 == 4:
-            res += "\t</tr>\n"
+    for i_row in range(5):
+        res += "\t<tr>\n"
+
+        for i_col in range(5):
+            single_entry = card_entries[i_col][i_row].split(" ", 1)
+            res += (
+                "\t\t<td>"
+                + "<b>{}</b><br>{}".format(single_entry[0], single_entry[1])
+                + "</td>\n"
+            )
+
+        res += "\t</tr>\n"
     res += "</table>\n"
     return res
 
-out_file = open(sys.argv[2], 'w')
-out_file.write(head)
-cards = int(sys.argv[3])
-for i in range(cards):
-    random.shuffle(terms)
-    if i != cards - 1:
-        out_file.write(generateTable(terms))
-    else:
-        out_file.write(generateTable(terms, False))
-out_file.write("</body></html>")
 
+# XHTML4 Strict, y'all!
+head = (
+    '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n'
+    '<html lang="en">\n<head>\n'
+    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n'
+    "<title>Bingo Cards</title>\n"
+    '<style type="text/css">\n'
+    "\tbody { font-size: 14px; }\n"
+    "\ttable { margin: 40px auto; border-spacing: 2px; }\n"
+    "\t.newpage { page-break-after:always; }\n"
+    "\ttr { height: 90px; }\n"
+    "\ttd { text-align: center; border: thin black solid; padding: 10px; width: 90px; }\n"
+    "</style>\n</head>\n<body>\n"
+)
+
+out_file = open(out_file_path, "w")
+out_file.write(head)
+
+n_card_entries = len(card_entries)
+number_terms_per_column = math.floor(n_card_entries / 5)
+terms_b = card_entries[0:number_terms_per_column]
+terms_i = card_entries[number_terms_per_column : number_terms_per_column * 2]
+terms_n = card_entries[2 * number_terms_per_column : 3 * number_terms_per_column]
+terms_g = card_entries[3 * number_terms_per_column : 4 * number_terms_per_column]
+terms_o = card_entries[4 * number_terms_per_column :]
+
+for i in range(cards):
+
+    shuffled_bingo_terms_list = [
+        random.sample(terms_b, len(terms_b))[:5],
+        random.sample(terms_i, len(terms_i))[:5],
+        random.sample(terms_n, len(terms_n))[:5],
+        random.sample(terms_g, len(terms_g))[:5],
+        random.sample(terms_o, len(terms_o))[:5],
+    ]
+
+    if ((i + 1) % 2) == 0:
+        out_file.write(generate_card(shuffled_bingo_terms_list))
+    else:
+        out_file.write(generate_card(shuffled_bingo_terms_list, False))
+
+out_file.write("</body></html>")
 out_file.close()
